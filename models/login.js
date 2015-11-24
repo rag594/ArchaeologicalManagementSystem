9aa59b416486c78115f799ecc3ffc1ac73f79631
rag594 <raghavrastogi594@gmail.com>
2015-11-24 18:01:07 +0530

    login route and model fixed

--- models/login.js
+++ models/login.js
@@ -4,24 +4,37 @@ var cfg = require('../config');
 var conn = mysql.createConnection(cfg.mysql);
 
 
-function getUserDetails(cb) {
+function getUserDetails(param, cb) {
 
     cfg.pool.getConnection(function (err, conn) {
 
         if (err) {
+
             res.json({"code": 100, "status": "Error in connection database"});
             return;
         }
 
-        var query = "Select username,password from login";
-        conn.query(query, function (err, rows) {
+        var query = "Select * from login where username = ? and password = ?";
+
+        conn.query(query, [param.username, param.password], function (err, rows) {
             conn.release();
+
             if (err) {
+
                 console.log(err);
                 cb(err, null);
             }
+
+            else if (rows[0]) {
+
+                console.log(rows[0]);
+                cb(null, rows[0]);
+            }
+
             else {
-                cb(null, rows);
+
+                cb("user not found!!!", null);
+
             }
         });
     });
--- routes/index.js
+++ routes/index.js
@@ -2,31 +2,27 @@ var express = require('express');
 var router = express.Router();
 var login = require('../models/login');
 
-/* GET home page. */
-//router.get('/', function(req, res, next) {
-//  res.render('index', { title: 'Express' });
-//});
+router.get('/', function (req, res, next) {
 
-router.get('/home', function (req,res,next) {
-
-  res.render('index');
+    res.render('index');
 });
 
 
-router.post('/home', function (req,res,next) {
-    console.log(req.body);
-    var user_name = req.body.username;
-    var pass = req.body.password;
-  login.getUserDetails(function(err,login){
+router.post('/', function (req, res, next) {
 
-    if(login.username == user_name && login.password == pass){
-      res.json({msg:"user found",s:1});
-    }
-    else{
-      res.json({msg:"password mismatch or user not found",s:0});
-    }
+    login.getUserDetails(req.body, function (err, login) {
 
-  });
+
+        if (login) {
+
+            res.json({msg: "user found", s: 1});
+        }
+        else {
+
+            res.json({msg: "password mismatch or user not found", s: 0});
+        }
+
+    });
 });
 
 module.exports = router;
